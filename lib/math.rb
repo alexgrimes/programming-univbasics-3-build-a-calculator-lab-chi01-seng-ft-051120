020b7e480a65cd81fe78f79354e0360dc3725f37
Alex S Grimes <alex@alexgrimes.com>
2020-03-28 23:12:15 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,16 +8,20 @@ end
 
 def division(num1, num2)
   division = (num1 / num2.to_i)
+  puts division
 end
 
 def multiplication(num1, num2)
   multiplication = num1 * num2
+  puts multiplication
 end
 
 def modulo(num1, num2)
   modulo = (num2 / num1.to_i)
+  puts modulo
 end
 
 def square_root(num1)
   square_root = math.sqrt(num1) 
+  puts square_root
 end
